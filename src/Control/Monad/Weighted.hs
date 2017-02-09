59ca0a7344115368bed5f591c68e04ac5d3bc349
Donnacha Oisín Kidney <mail@doisinkidney.com>
2017-02-09 17:52:41 +0000

    Instances

--- src/Control/Monad/Weighted.hs
+++ src/Control/Monad/Weighted.hs
@@ -9,15 +9,22 @@ module Control.Monad.Weighted
   ,pattern WeightedT
   ,Weighted
   ,runWeighted
-  ,pattern Weighted)
+  ,pattern Weighted
+  ,execWeightedT
+  ,evalWeightedT
+  ,execWeighted
+  ,evalWeighted)
   where
 
+import           Control.Applicative
 import           Control.Monad.Identity
 import           Control.Monad.State.Strict
 import           Control.Monad.Writer
 import           Data.Coerce
 import           Data.Semiring
 
+-- | A monad transformer similar to 'WriterT', except that it does not leak
+-- space, and it uses the 'Semiring' class, rather than 'Monoid'.
 newtype WeightedT s m a =
     WeightedT_ (StateT s m a)
     deriving (Functor,Applicative,Monad,MonadTrans)
@@ -56,3 +63,33 @@ instance (Semiring s, Monad m) => MonadWriter (Mul s) (WeightedT s m) where
   pass (WeightedT_ s) = WeightedT_ (passS s) where
     passS = (=<<) (uncurry (<$) . fmap (modify . coerce))
   {-# INLINE pass #-}
+
+evalWeightedT :: (Monad m, Semiring s) => WeightedT s m a -> m a
+evalWeightedT =
+    (coerce :: (StateT s m a -> m a) -> WeightedT s m a -> m a)
+        (`evalStateT` one)
+{-# INLINE evalWeightedT #-}
+
+execWeightedT :: (Monad m, Semiring s) => WeightedT s m a -> m s
+execWeightedT =
+    (coerce :: (StateT s m a -> m s) -> WeightedT s m a -> m s)
+        (`execStateT` one)
+{-# INLINE execWeightedT #-}
+
+evalWeighted :: Semiring s => Weighted s a -> a
+evalWeighted =
+    (coerce :: (State s a -> a) -> Weighted s a -> a)
+        (`evalState` one)
+{-# INLINE evalWeighted #-}
+
+execWeighted :: Semiring s => Weighted s a -> s
+execWeighted =
+    (coerce :: (State s a -> s) -> Weighted s a -> s)
+        (`execState` one)
+{-# INLINE execWeighted #-}
+
+instance (Alternative m, Monad m, Semiring s) => Alternative (WeightedT s m) where
+  empty = WeightedT empty
+  WeightedT x <|> WeightedT y = WeightedT (x <|> y)
+  _ <|> _ = undefined
+
